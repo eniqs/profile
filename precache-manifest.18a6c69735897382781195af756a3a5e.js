05fb8dd2662c5f5c94de3de014e67268b91bf0c7
Olaoye Enikanorogbon <olaoye.enikanorogbon@egym.de>
2020-04-21 11:20:41 +0200

    Updates

---
+++ precache-manifest.18a6c69735897382781195af756a3a5e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704a474d4bd2c7a82fbcc83ec6ab578c",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "7c05c90c1cfffe58839f",
+    "url": "/profile/static/css/main.b02c1b6f.chunk.css"
+  },
+  {
+    "revision": "837c5a7417c855e64ff8",
+    "url": "/profile/static/js/2.15bc528b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/profile/static/js/2.15bc528b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c05c90c1cfffe58839f",
+    "url": "/profile/static/js/main.eac8d878.chunk.js"
+  },
+  {
+    "revision": "182a4400b0e948c43a5c",
+    "url": "/profile/static/js/runtime-main.fd97c823.js"
+  },
+  {
+    "revision": "ab11f9540e0d550e941055a26374925c",
+    "url": "/profile/static/media/bg.ab11f954.jpg"
+  },
+  {
+    "revision": "f39aaebd4745954833e2f3eb807efa5f",
+    "url": "/profile/static/media/bg1.f39aaebd.jpg"
+  },
+  {
+    "revision": "f39aaebd4745954833e2f3eb807efa5f",
+    "url": "/profile/static/media/bg8.f39aaebd.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/profile/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/profile/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/profile/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/profile/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "134674eaeb0487a6baf6f2fe7e5212a9",
+    "url": "/profile/static/media/profilePic.134674ea.jpg"
+  }
+]);
